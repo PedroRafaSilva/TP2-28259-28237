6ec8c93b19085b02e2f1be66c8b8fd4d89ce16d5
PedroRafaSilva <pedrosedry@gmail.com>
2023-06-09 21:21:28 +0100

    Menu Remade

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,22 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
-
-
 {
    public void PlayHuman()
-{
-    // Carrega a próxima cena
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-}
-
-
-   public void PlayIA() {
-    
-    // Carrega a próxima cena
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   {
+      // Carrega a próxima cena
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
-
+   
    public void Exit() {
       Application.Quit();
    }
